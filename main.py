536986a691950fdb2ce66ddff971a0d0165170ed
ricardo <hakdigital98@gmail.com>
2022-04-14 21:21:34 -0500

    checkout validaciones_Query_parameters

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from typing import Optional
 #_pydantic________________
 from pydantic import BaseModel
 #fastAPI___________________
-from fastapi import FastAPI
-from fastapi import Body
+from fastapi import FastAPI, Query
+from fastapi import Body#validacion obligatoria
+from fastapi import Query# validaciones Opcionales en parametros
 
 app = FastAPI()
 
@@ -21,8 +22,17 @@ class person(BaseModel):
 def home():
     return{"hello":"world"}
 
-    
+
 #_________request and response  body_______________________________________
 @app.post("/person/new")
-def create_person(person: person = Body(...)):
+def create_person(person: person = Body(...)):# se creea un modelo de la class person y se representa en (def con person )
     return person
+
+
+#_____Validaciones: Query Parameters_______________________________________
+@app.get("/person/detail")
+def show_person(
+    name:Optional[str] =Query (None, min_length=1,max_length=50),
+    age : str = Query(...)
+):
+    return{name:age}
